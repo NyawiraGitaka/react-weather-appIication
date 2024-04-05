756a3592dc826d9de453adcf43427fb7b0be10a3
NyawiraGitaka <nyawira.gitaka@gmail.com>
2024-04-05 17:02:25 +0300

    Addition of Github repository URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-
-import './App.css';
+import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      Weather Application
+      <footer>
+        This project was coded by Nyawira Gitaka and is 
+        <a
+          href="https://github.com/NyawiraGitaka/react-weather-appIication"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
