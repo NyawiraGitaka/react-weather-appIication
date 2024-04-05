ad29ad894577d96dff4183ee435449ecdbe4499a
NyawiraGitaka <nyawira.gitaka@gmail.com>
2024-04-05 22:48:03 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather Application
-      <footer>
-        This project was coded by Nyawira Gitaka and is 
-        <a
-          href="https://github.com/NyawiraGitaka/react-weather-appIication"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Application</h1>
+        <footer>
+          This project was coded by Nyawira Gitaka and is{" "}
+          <a
+            href="https://github.com/NyawiraGitaka/react-weather-appIication"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
